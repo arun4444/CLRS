e9522c0a0cb3e3c3f82f3f76b1b9db88b35219a3
Arun Krishnan <arun4444@gmail.com>
2021-10-10 14:30:40 -0500

    C implimentation of insertionSort

--- Sorting/InsertionSort/C/insertionSort.c
+++ Sorting/InsertionSort/C/insertionSort.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#define ARRAY_SIZE 14
 
 void sort(int *toSort, int arraySize);
 
 int main()
 {
-   int toSort[14] = {2, 12, 3, 9, 9, 0, 8, 4, 132, 23, 19999, 9, 9, 0};
-   sort(toSort,14);
+   int toSort[ARRAY_SIZE] = {2, 12, 3, 9, 9, 0, 8, 4, 132, 23, 19999, 9, 9, 0};
+   sort(toSort, ARRAY_SIZE);
    int loop;
-   for(loop = 0; loop < 14; loop++){
+   for (loop = 0; loop < ARRAY_SIZE; loop++)
+   {
       printf("%d ", toSort[loop]);
    }
    return 0;
@@ -20,7 +22,8 @@ void sort(int *toSort, int arraySize)
    {
       int key = toSort[j];
       int i = j - 1;
-      while (i >= 0 && toSort[i] > key){
+      while (i >= 0 && toSort[i] > key)
+      {
          toSort[i + 1] = toSort[i];
          i--;
       }
